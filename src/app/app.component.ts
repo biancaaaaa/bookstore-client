3af009fd0b1d2bd49e421536f71ab8486a43f6ae
Bianca <bianca.hoellmueller@gmx.net>
2019-05-03 20:00:35 +0200

    upload all files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Book} from "./shared/class/book";
+import {AuthService} from "./shared/service/authentication.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'bs-root',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class AppComponent {
-  title = 'bookstore19';
+  book: Book;
+
+  constructor(private authService: AuthService,
+              private router: Router,
+              private route: ActivatedRoute) {}
+
+  bookSelected(book: Book) {
+    this.router.navigate(['../books', book.isbn], {relativeTo: this.route});
+  }
+
+  /**
+   * checks, if there is a user logged in.
+   */
+  isLoggedIn() {
+    return this.authService.isLoggedIn();
+  }
+
+  /**
+   * checks, if current user is an admin.
+   */
+  isAdmin() {
+    return this.authService.isAdmin();
+  }
+
+  /**
+   * returns login label
+   */
+  getLoginLabel() {
+    return this.isLoggedIn() ? 'Logout' : 'Login';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,78 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BookListComponent} from './book-list/book-list.component';
+import {BookListItemComponent} from './book-list-item/book-list-item.component';
+import {BookDetailsComponent} from './book-details/book-details.component';
+import {BookStoreService} from "./shared/service/book-store.service";
+import {HomeComponent} from './home/home.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {BookFormComponent} from './book-form/book-form.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {SearchComponent} from './search/search.component';
+import {DateValueAccessorModule} from "angular-date-value-accessor";
+import {LoginComponent} from './login/login.component';
+import {AuthService} from "./shared/service/authentication.service";
+import {TokenInterceptorService} from "./shared/service/token-interceptor.service";
+import {JwtInterceptorService} from "./shared/service/jwt-interceptor.service";
+import { ShoppingBasketComponent } from './shopping-basket/shopping-basket.component';
+import { AddToCartComponent } from './add-to-cart/add-to-cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { UserOrdersComponent } from './user-orders/user-orders.component';
+import {OrderService} from "./shared/service/order.service";
+import { DeliveryAddressComponent } from './delivery-address/delivery-address.component';
+import { OrderOverviewComponent } from './order-overview/order-overview.component';
+import {CurrencyPipe} from "./shared/pipe/currency-pipe";
+import { AllOrdersComponent } from './all-orders/all-orders.component';
+import { OrderDetailsComponent } from './order-details/order-details.component';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencyPipe,
+    BookListComponent,
+    BookListItemComponent,
+    BookDetailsComponent,
+    HomeComponent,
+    BookFormComponent,
+    SearchComponent,
+    LoginComponent,
+    ShoppingBasketComponent,
+    AddToCartComponent,
+    CheckoutComponent,
+    UserOrdersComponent,
+    DeliveryAddressComponent,
+    OrderOverviewComponent,
+    AllOrdersComponent,
+    OrderDetailsComponent,
+    OrderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    DateValueAccessorModule
   ],
-  providers: [],
+  providers: [
+    BookStoreService,
+    OrderService,
+    AuthService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptorService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
